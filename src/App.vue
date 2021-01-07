df99856392d5b2f1391d8f7cbfa026c9402a0589
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-01-06 23:24:34 -0800

    home page impliment

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,68 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    
+    >
+     <v-list
+          dense
+          rounded
+        >
+          <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+            :to="item.to"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title >{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+    <v-app-bar 
+      app  
+      color="#78909C"
+      dark
+      >
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title >Movie Rating App</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm--and-down">
+        <v-btn flat v-bind:to="{name : 'AddMovie'}">Add Movie</v-btn>
+      </v-toolbar-items>
+    </v-app-bar>
+    <v-content>
+      <v-container fluid>
+        <div id="app">
+          <router-view></router-view>
+        </div>
+      </v-container>
+    </v-content>
+    <v-footer app color="#B0BEC5">
+      <span class="purple--text ma-auto">&copy; Dipu, 2021</span>
+    </v-footer>
+
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+  export default {
+    data: () => ({
+       drawer: null,
+         items: [
+          { title: 'Home', icon: 'mdi-home', to:'/' },
+          { title: 'Contact', icon: 'mdi-contacts', to:'/contact' },
+        ],
+        right: null,
+       
+    })
   }
-}
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
+  el:'#app',
   router,
   store,
+  vuetify,
+  components:{App},
+  template:'<App/>',
   render: function (h) { return h(App) }
 }).$mount('#app')
