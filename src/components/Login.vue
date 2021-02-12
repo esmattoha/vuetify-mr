673c38c88b2b64360c2bce8582f5ad34a83f7b7d
Esmattoha Mondal <72984742+esmattoha@users.noreply.github.com>
2021-02-12 14:27:47 -0800

    store token in localStorage

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -10,7 +10,6 @@
        <v-text-field
           v-model="password"
           label="Password"
-          :rules="emailRule"
           required
         ></v-text-field>
        
@@ -27,55 +26,55 @@
 </template>
 
 // <script>
-// import axios from 'axios';
-// export default{
-//     data: ()=>({
-//         email:'',
-//         password:'',
+import axios from 'axios';
+export default{
+    data: ()=>({
+        email:'',
+        password:'',
 
-//         emailRule:[
-//             v => !!v || "Email is required ",
-//             v => /\S+@\S+/.test(v) || "Email must be valid",
-//         ],
-//     }),
-//     methods:{
-//         async submit(){
-//             if(this.$refs.form.validate()){
-//                 return axios({
-//                     method:'POST',
-//                     data:{
-//                         username: this.username,
-//                         email: this.email,
-//                         password: this.password
-//                     },
-//                     url:'http://localhost:8081/user/register',
-//                     headers:{
-//                         'Content-Type':'application/json',
-//                     },
-//                 })
-//                  .then(()=>{
-//                     this.$swal(
-//                         'Great',
-//                         'You have been successfully registered!',
-//                         'success'
-//                     )
-//                     this.$router.push({name:"Login"});
+        emailRule:[
+            v => !!v || "Email is required ",
+            v => /\S+@\S+/.test(v) || "Email must be valid",
+        ],
+    }),
+    methods:{
+        async submit(){
+            if(this.$refs.form.validate()){
+                return axios({
+                    method:'POST',
+                    data:{
+                        email: this.email,
+                        password: this.password
+                    },
+                    url:'http://localhost:8081/user/register',
+                    headers:{
+                        'Content-Type':'application/json',
+                    },
+                })
+                 .then((responce)=>{
+                     window.localStorage.setItem('auth', responce.data.token);
+                    this.$swal(
+                        'Great',
+                        'You have been successfully Login!',
+                        'success'
+                    )
+                    this.$router.push({name:"Home"});
                    
-//                 })
-//                 .catch((error)=>{
-//                     const message = error.responce.data.message;
-//                     this.$swal(
-//                         'Oh ooppss!',
-//                        `${message}`,
-//                         'error'
-//                     )
-//                 });
-//             }
-//             return true;
-//         },
-//         clear(){
-//             this.$refs.form.reset();
-//         }
-//     }
-// }
-// </script>
+                })
+                .catch((error)=>{
+                    const message = error.responce.data.message;
+                    this.$swal(
+                        'Oh ooppss!',
+                       `${message}`,
+                        'error'
+                    )
+                });
+            }
+            return true;
+        },
+        clear(){
+            this.$refs.form.reset();
+        }
+    }
+}
+</script>
